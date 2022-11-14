ec77029d53066d7e64e4de5097a30988234e4457
towhid7667 <towhidulhq@gmail.com>
2022-11-15 01:01:10 +0600

    ending commit

--- src/Pages/Blogs.js
+++ src/Pages/Blogs.js
@@ -6,10 +6,10 @@ const Blogs = () => {
           <Helmet>
             <title>Mad-kitchen-blogs</title>
         </Helmet>
-        <div className='max-w-screen-xl mx-auto'>
+        <div className='w-11/12 lg:w-10/12 md:w-10/12 mx-auto'>
             <div className="hero min-h-screen bg-base-200 my-5 p-10 rounded-lg ">
   <div className="hero-content flex-col lg:flex-row-reverse">
-    <img alt='' src="https://images.pexels.com/photos/5203849/pexels-photo-5203849.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="max-w-sm rounded-lg shadow-2xl" />
+    <img alt='' src="https://images.pexels.com/photos/5203849/pexels-photo-5203849.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className=" rounded-lg shadow-2xl" />
     <div>
       <h1 className="text-5xl font-bold">Difference between SQL and NoSQL</h1>
       <p className="py-6">SQL is the programming language used to interface with relational databases. (Relational databases model data as records in rows and tables with logical links between them). NoSQL is a class of DBMs that are non-relational and generally do not use SQL.</p>
@@ -18,7 +18,7 @@ const Blogs = () => {
 </div>
             <div className="hero min-h-screen bg-base-200 my-5 p-10 rounded-lg">
   <div className="hero-content flex-col lg:flex-row-reverse">
-    <img alt='' src="https://images.pexels.com/photos/3585089/pexels-photo-3585089.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="max-w-sm rounded-lg shadow-2xl" />
+    <img alt='' src="https://images.pexels.com/photos/3585089/pexels-photo-3585089.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="rounded-lg shadow-2xl" />
     <div>
       <h1 className="text-5xl font-bold">What is JWT, and how does it work?</h1>
       <p className="py-6">
@@ -28,7 +28,7 @@ What is JWT (JSON Web Token)? JSON Web Token (JWT) is an open standard (RFC 7519
 </div>
             <div className="hero min-h-screen bg-base-200 my-5 p-10 rounded-lg">
   <div className="hero-content flex-col lg:flex-row-reverse">
-    <img src="https://images.pexels.com/photos/5483075/pexels-photo-5483075.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="max-w-sm rounded-lg shadow-2xl" />
+    <img src="https://images.pexels.com/photos/5483075/pexels-photo-5483075.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className=" rounded-lg shadow-2xl" />
     <div>
       <h1 className="text-5xl font-bold">What is the difference between javascript and NodeJS?</h1>
       <p className="py-6">JavaScript is a simple programming language that can be used with any browser that has the JavaScript Engine installed. Node. js, on the other hand, is an interpreter or execution environment for the JavaScript programming language.</p>
@@ -37,7 +37,7 @@ What is JWT (JSON Web Token)? JSON Web Token (JWT) is an open standard (RFC 7519
 </div>
             <div className="hero min-h-screen bg-base-200 my-5 p-10 rounded-lg">
   <div className="hero-content flex-col lg:flex-row-reverse">
-    <img alt='' src="https://images.pexels.com/photos/5474282/pexels-photo-5474282.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="max-w-sm rounded-lg shadow-2xl" />
+    <img alt='' src="https://images.pexels.com/photos/5474282/pexels-photo-5474282.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="rounded-lg shadow-2xl" />
     <div>
       <h1 className="text-5xl font-bold">How does NodeJS handle multiple requests at the same time?</h1>
       <p className="py-6">How NodeJS handle multiple client requests? NodeJS receives multiple client requests and places them into EventQueue. NodeJS is built with the concept of event-driven architecture. NodeJS has its own EventLoop which is an infinite loop that receives requests and processes them.</p>
